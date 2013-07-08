6b672d361981cbc3e8bec7ad6b1ef6cf95eb7661
Baochuan Lu <lubaochuan@gmail.com>
2013-07-08 19:44:45 +0000

    Changed to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var buffer = fs.readFileSync('./index.html', function(err, data){
+    if (err) throw err;
+    console.log(data);
+  });
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
